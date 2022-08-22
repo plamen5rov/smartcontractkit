701819d0e1dd1cc9c8f3becd0ebb74cf496d4e8c
Plamen <biznetmen@gmail.com>
2022-08-22 12:31:47 +0300

    code reverted to previous state

--- hh-fcc/ethers-simple-storage/deploy.js
+++ hh-fcc/ethers-simple-storage/deploy.js
@@ -8,12 +8,12 @@ async function main() {
   // And make sure to have your ganache network up!
   let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-  //   encryptedJson,
-  //   process.env.PRIVATE_KEY_PASSWORD
-  // );
-  // wallet = wallet.connect(provider);
+  //const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  //let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //  encryptedJson,
+  //  process.env.PRIVATE_KEY_PASSWORD
+  //);
+  //wallet = wallet.connect(provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
